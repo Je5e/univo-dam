be2c71696315d47871aa7cfb33134cbe39db11c1
Jesé Nehemías Alvarado Salvador <jalvarado@univo.edu.sv>
2021-12-01 21:17:59 -0600

    Add Xamarin.Forms Shell solution

---
+++ ejercicios/Attendance/Attendance/Attendance/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Attendance.ViewModels;
+using Attendance.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Attendance
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
